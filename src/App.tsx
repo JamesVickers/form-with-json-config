5ae04b0ed3cc3792c30ded57c3a25b65b967ccb9
James Vickers <jamesvickers@jamess-imac.home>
2023-12-18 15:38:09 +0000

    FIX: remove props from StaticForm to stop rerender issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./styles.css";
-
 import StaticForm from "./StaticForm";
-import { getData } from "./utils";
 
 export default function App() {
   return (
     <div className="App" style={{ backgroundColor: "grey" }}>
       <h1>Static Renderer</h1>
-      <StaticForm data={getData()} />
+      <StaticForm />
     </div>
   );
 }
