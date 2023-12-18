cb2c899cb346ebd2e703024b30633687f1d3e203
James Vickers <jamesvickers@jamess-imac.home>
2023-12-18 15:39:49 +0000

    FEAT: working dynamic form values with default values and save updated values as output

--- src/StaticForm.tsx
+++ src/StaticForm.tsx
@@ -1,18 +1,19 @@
-import React, { ChangeEventHandler, useState } from "react";
+import React, { ChangeEventHandler, useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import { Button, Grid, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import { InputItem } from "./types";
+import { InputItem, TextInput, EnumInput, IntegerInput } from "./types";
 // import { setDeep } from "./utils";
+import { getData } from "./utils";
 
-type StaticFormProps = {
-  data: InputItem[];
-};
-
-const StaticForm = ({ data }: StaticFormProps): JSX.Element => {
-  console.log('StaticForm > data: ', data)
-  const [localData, setLocalData] = useState<InputItem[]>(data);
+const StaticForm = (): JSX.Element => {
+  const [localData, setLocalData] = useState<InputItem[]>([]);
   const [savedObject, setSavedObject] = useState<InputItem[]>();
+
+  useEffect(() => {
+    const data = getData();
+    setLocalData(data)
+  }, [])
 
   const onChange = (
     path: string,
@@ -35,53 +36,41 @@ const StaticForm = ({ data }: StaticFormProps): JSX.Element => {
     });
   };
 
-  // const onChange =
-  // (
-  //   path: string,
-  // ): ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> =>
-  // (event) =>
-  //   setLocalData(setDeep(localData, path, event.target.value));
-
   const onSaveChanges = () => {
     setSavedObject(localData);
   };
+
+  const isValidProperty = (obj: object, prop: string): prop is keyof typeof obj => {
+    return prop in obj;
+  }
+
+  const getValue = (item: InputItem) => {
+    const newValue = (isValidProperty(item, item.path) && item[item.path as keyof InputItem] !== undefined)
+      ? item[item.path as keyof InputItem] as string
+      : ('defaultValue' in item)
+        ? item.defaultValue ?? ''
+        : ''
+    return newValue;
+  }
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Paper variant="elevation" style={{ margin: "10px" }}>
           <Typography variant="h4">Vehicle details</Typography>
-          {data.map((property, index) => (
+          {localData && localData.map((item, index) => (
             <div key={index}>
-              <label htmlFor={property.label}>{property.label}</label>
+              <label htmlFor={item.label}>{item.label}</label>
               <input
-                type={property.type}
-                id={property.label}
-                name={property.label}
-                value={('defaultValue' in property) ? property.defaultValue ?? '' : ''}
-                onChange={onChange(property.path, index)}
+                type={item.type}
+                id={item.label}
+                name={item.label}
+                value={getValue(item)}
+                onChange={onChange(item.path, index)}
               />
             </div>
           )
           )}
-          {/* <Grid container alignItems="flex-start" spacing={2} padding={5}>
-            <Grid item xs={12}>
-              <TextField
-                style={{ width: "100%" }}
-                label="Driver Name"
-                value={localData.driver?.name || ""}
-                onChange={onChange("driver.name")}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                style={{ width: "100%" }}
-                label="Vehicle Regplate"
-                value={localData.vehicle?.regplate || ""}
-                onChange={onChange("vehicle.regplate")}
-              />
-            </Grid>
-          </Grid> */}
           <Grid item xs={12}>
             <div style={{ margin: "10px", padding: "10px" }}>
               <Button variant="outlined" onClick={onSaveChanges}>
