6a95111750b6525d65a2510395e80f17a8c75d0e
James Vickers <jamesvickers@jamess-imac.home>
2023-12-18 13:30:59 +0000

    CHORE: add new types and interfaces based on example json files

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,39 @@
-type ObjectAttributte = Record<string, unknown>;
-export type MyObject = Record<string, ObjectAttributte>;
+type CurrencyInputValue =  'EUR' | 'USD' | 'GBP';
+type EnumInputValue = 'yes' | 'no' | 'maybe';
+
+interface TextInput {
+    type: 'textInput';
+    label: string;
+    path: string;
+    defaultValue?: string;
+  }
+  
+  interface EnumInput {
+    type: 'enumInput';
+    label: string;
+    path: string;
+    values: EnumInputValue[];
+    defaultValue?: string;
+  }
+  
+  interface IntegerInput {
+    type: 'integerInput';
+    label: string;
+    path: string;
+    min?: number;
+    max?: number;
+    defaultValue?: number;
+  }
+  
+  interface CurrencyInput {
+    type: 'currencyInput';
+    label: string;
+    path: string;
+    min?: number;
+    max?: number;
+    currencies: CurrencyInputValue[];
+  }
+
+export type InputItem = TextInput | EnumInput | IntegerInput | CurrencyInput;
+
+type ObjectAttribute = Record<string, unknown>;
